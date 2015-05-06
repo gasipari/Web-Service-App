17b6b15691a02aeca40fadaa5ff3546103e2e0c2
Marius Muk <riamarius@gmail.com>
2015-05-06 11:29:38 -0400

    Added AFNetworking networking framework, bridge file (for object-c to work with swift) and Facebook's Graph API call

--- Web Service Example/ViewController.swift
+++ Web Service Example/ViewController.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+         let manager = AFHTTPRequestOperationManager()
+        
+        manager.GET( "http:/graph.facebook.com/bobdylan",
+            parameters: nil,
+            success: /* begin of closure ?? */{ (operation: AFHTTPRequestOperation!,responseObject: AnyObject!) in
+                /* begin of closure ?? */
+                println("Response: " + responseObject.description)
+                
+                if let myName = responseObject["name"] as? String {
+                    self.myNameLabel.text = myName
+                }
+                
+            }, /* end of closure */
+            failure: { (operation: AFHTTPRequestOperation!,error: NSError!) in
+                println("Error: " + error.localizedDescription)
+        })
+        
     }
 
     override func didReceiveMemoryWarning() {
---
+++ bridge.h
@@ -0,0 +1,15 @@
+//
+//  bridge.h
+//  Web Service Example
+//
+//  Created by Marius on 5/6/15.
+//  Copyright (c) 2015 Marius Mukunzi. All rights reserved.
+//
+
+#ifndef Web_Service_Example_bridge_h
+#define Web_Service_Example_bridge_h
+
+
+#endif
+
+#import "AFNetworking/AFNetworking.h"
